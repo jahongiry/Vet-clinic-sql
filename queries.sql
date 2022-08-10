ce3d61f1c5947b4a2bd8b0f60bb9ff85f8a31015
Jahongir Yusupov <jahongir.yusupov.94@icloud.com>
2022-08-10 14:23:30 +0500

    Add weight species escape_attempts queries

--- queries.sql
+++ queries.sql
@@ -6,3 +6,10 @@ SELECT name, escape_attempts from animals WHERE weight_kg > 10.5;
 SELECT * from animals WHERE neutered = 't';
 SELECT * from animals WHERE name <> 'Gabumon';
 SELECT * from animals WHERE weight_kg BETWEEN 10.4 AND 17.3;
+
+SELECT COUNT(*) FROM animals;
+SELECT COUNT(*) FROM animals WHERE escape_attempts = 0;
+SELECT AVG(weight_kg) FROM animals;
+SELECT COUNT(escape_attempts), neutered  FROM animals GROUP BY neutered;
+SELECT MAX(weight_kg) AS maximum_weight, MIN(weight_kg) AS minimum_weight, species FROM animals GROUP BY species;
+SELECT AVG(escape_attempts), species FROM animals WHERE date_of_birth BETWEEN '1990-01-01' AND '2000-12-31' GROUP BY species;
