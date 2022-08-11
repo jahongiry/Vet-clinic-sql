6222353d97e010bc1766396ba570af606b312e47
Jahongir Yusupov <jahongir.yusupov.94@icloud.com>
2022-08-12 03:51:36 +0500

    Add data to owner's and species table

--- data.sql
+++ data.sql
@@ -43,8 +43,3 @@ UPDATE animals SET owner_id = 2 WHERE name IN( 'Gabumon', 'Pikachu');
 UPDATE animals SET owner_id = 3 WHERE name IN( 'Devimon', 'Plantmon');
 UPDATE animals SET owner_id = 4 WHERE name IN( 'Charmander', 'Squirtle', 'Blossom');
 UPDATE animals SET owner_id = 5 WHERE name IN( 'Angemon', 'Boarmon');
-
-
-
-
-
