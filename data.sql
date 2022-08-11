578b1704a52f287bc656c6bbba3456ddfcc73873
Jahongir Yusupov <jahongir.yusupov.94@icloud.com>
2022-08-12 03:50:38 +0500

    Update data.sql

--- data.sql
+++ data.sql
@@ -14,3 +14,37 @@ VALUES
 (9, 'Boarmon', '2005-06-07', 20.4, TRUE, 7),
 (10, 'Blossom', '1998-10-13', 17, TRUE, 3),
 (11, 'Ditto', '2022-05-14', 22, TRUE, 4);
+
+/* Owner's table data */
+INSERT INTO owners (full_name, age)
+VALUES
+('Sam Smith', 34),
+('Jennifer Orwell', 19),
+('Bob', 45),
+('Melody Pond', 77),
+('Dean Winchester', 14),
+('Jodie Whittaker', 38);
+
+/* Species's table data */
+INSERT INTO species (name)
+VALUES
+('Pokemon'),
+('Digimon');
+
+-- Modify your inserted animals so it includes the species_id value:
+-- If the name ends in "mon" it will be Digimon
+-- All other animals are Pokemon
+UPDATE animals SET species_id = 2 WHERE name LIKE '%mon';
+UPDATE animals SET species_id = 1 WHERE species_id IS NULL;
+
+/* Update the owner_id in animals */
+UPDATE animals SET owner_id = 1 WHERE name = 'Agumon';
+UPDATE animals SET owner_id = 2 WHERE name IN( 'Gabumon', 'Pikachu');
+UPDATE animals SET owner_id = 3 WHERE name IN( 'Devimon', 'Plantmon');
+UPDATE animals SET owner_id = 4 WHERE name IN( 'Charmander', 'Squirtle', 'Blossom');
+UPDATE animals SET owner_id = 5 WHERE name IN( 'Angemon', 'Boarmon');
+
+
+
+
+
